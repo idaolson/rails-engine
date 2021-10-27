963c18701afb34696105dc1720399a85a772d779
Ida Olson <idaolson@gmail.com>
2021-10-26 18:53:03 -0600

    Create items controller with index and merchant_items_index methods

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::ItemsController < ApplicationController
+  def index
+    if params[:merchant_id]
+      merchant_items_index(params[:merchant_id])
+    else
+      per_page = 20
+      page = 1
+      items = Item.all.paginate(per_page, page)
+      render json: ItemSerializer.new(items)
+    end
+  end
+
+  def merchant_items_index(merchant_id)
+    if Merchant.exists?(merchant_id)
+      items = Item.where(merchant_id: merchant_id)
+      render json: ItemSerializer.new(items)
+    else
+      render json: { error: 'Not Found' }, status: :not_found
+    end
+  end
+end
