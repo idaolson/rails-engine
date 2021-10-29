d0c3e34fbe71b884972fa346157a1582ad46a864
Ida Olson <idaolson@gmail.com>
2021-10-29 05:56:18 -0600

    Update models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,8 +3,4 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
-
-  validates :status, :customer_id, presence: true
-
-  enum status: { :cancelled => 0, 'in progress' => 1, :completed => 2 }
 end
