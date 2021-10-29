b15c9b0abe77be7b9e1da54bebec0ec2f9776dd9
Ida Olson <idaolson@gmail.com>
2021-10-29 02:33:27 -0600

    Add method most items

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,9 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
-  # has_many :invoices, through: :items
-  # has_many :invoice_items, through: :items
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices
 
   validates :name, presence: true
 
@@ -9,5 +11,14 @@ class Merchant < ApplicationRecord
     where('name ILIKE ?', "%#{query}%")
       .order(name: :asc)
       .first
+  end
+
+  def self.most_items(quantity = 5)
+    joins(items: {invoice_items: {invoice: :transactions}})
+    .select("merchants.*, SUM(invoice_items.quantity) AS count")
+    .where("transactions.result = ?", "success")
+    .group("merchants.id")
+    .order("count desc")
+    .limit(quantity)
   end
 end
