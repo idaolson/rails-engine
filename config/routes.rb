4c2f1f05e7ed4c4180639b6a918a93b0e94b7350
Ida Olson <idaolson@gmail.com>
2021-10-29 04:30:13 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      # section two
       get '/merchants/find', to: 'merchants#find'
       get '/items/find_all', to: 'items#find_all'
+
+      # section three
+      get '/revenue/merchants/:id', to: 'revenue#total_revenue_merchant'
       get '/merchants/most_items', to: 'merchants#most_items_sold'
       get '/revenue/items', to: 'revenue#most_revenue_items'
 
-
+      # section one
       resources :merchants, only: %i[index show] do
         resources :items, only: :index
       end
