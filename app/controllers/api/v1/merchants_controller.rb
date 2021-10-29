912c7f987c44e3b30a752ab003fc21f808164d55
Ida Olson <idaolson@gmail.com>
2021-10-28 19:46:04 -0600

    Fix method i broke

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,15 +1,20 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-
     per_page = params[:per_page].try(:to_i) || 20
     page = params[:page].try(:to_i) || 1
-
     merchants = Merchant.all.page_number(per_page, page)
     render json: MerchantSerializer.new(merchants)
   end
 
   def show
-    merchant = Merchant.find(params[:id])
-    render json: MerchantSerializer.new(merchant)
+    if params[:item_id] && Item.exists?(params[:item_id])
+      merchant = Item.find(params[:item_id]).merchant
+      render json: MerchantSerializer.new(merchant)
+    elsif params[:id] && Merchant.exists?(params[:id])
+      merchant = Merchant.find(params[:id])
+      render json: MerchantSerializer.new(merchant)
+    else
+      render json: { error: 'Not Found' }, status: :not_found
+    end
   end
 end
