52674509562983008f4d564dfbec171b897f412a
Ida Olson <idaolson@gmail.com>
2021-10-28 18:43:58 -0600

    Rename method to be more clear

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def self.paginate(per_page, page)
+  def self.page_number(per_page, page)
     page = 1 if page <= 0
     limit(per_page).offset(per_page * (page - 1))
   end
