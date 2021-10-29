cdfdfbbffc0f478996599901e1559fd6e6c1e78b
Ida Olson <idaolson@gmail.com>
2021-10-28 20:11:59 -0600

    Add additional tests for sad paths

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -28,10 +28,68 @@ RSpec.describe "items requests" do
       expect(all_items_response[:data][0][:attributes]).to_not have_key :created_at
       expect(all_items_response[:data][0][:attributes]).to_not have_key :updated_at
     end
+
+    it 'gets 20 items and page 1 by default' do
+      merchant = create(:merchant)
+      create_list(:item, 23, merchant: merchant)
+
+      get '/api/v1/items'
+
+      expect(response).to be_successful
+
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items[:data].count).to eq(20)
+      expect(items[:data].first[:id].to_i).to eq(Item.first.id)
+    end
+
+    it 'can take per page and page number params' do
+      merchant = create(:merchant)
+      create_list(:item, 23, merchant: merchant)
+
+      get '/api/v1/items', params: { per_page: 10, page: 2 }
+
+      expect(response).to be_successful
+
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items[:data].count).to eq(10)
+      expect(items[:data].first[:id].to_i).to eq(Item.offset(10).first.id)
+    end
+
+    it 'gets page one if page number params are less that one or not an integer' do
+      merchant = create(:merchant)
+      create_list(:item, 23, merchant: merchant)
+
+      get '/api/v1/items', params: { page: 0 }
+
+      expect(response).to be_successful
+
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items[:data].count).to eq(20)
+      expect(items[:data].first).to have_key :id
+
+      get '/api/v1/items', params: { page: -5 }
+
+      expect(response).to be_successful
+
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items[:data].count).to eq(20)
+
+      get '/api/v1/items', params: { page: 'page 8' }
+
+      expect(response).to be_successful
+
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items[:data].count).to eq(20)
+    end
   end
 
   describe "Get /api/v1/items/:id" do
-    it "gets all data for 1 merchant" do
+    it "gets all data for 1 item" do
       merchant = create(:merchant)
       item = create(:item, merchant: merchant)
 
