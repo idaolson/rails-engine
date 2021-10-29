c801e60a6b8d8aaf4cd5642c07de3303a9585649
Ida Olson <idaolson@gmail.com>
2021-10-29 01:29:59 -0600

    Add find by price method to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,5 +7,11 @@ class Item < ApplicationRecord
   def self.find_by_name(query)
     where('name ILIKE ?', "%#{query}%")
       .order(name: :asc)
-  end 
+  end
+
+  def self.find_by_price(min_price = nil, max_price = nil)
+    min_price = 0 if min_price.nil?
+    max_price = Item.maximum(:unit_price) if max_price.nil?
+    Item.where('unit_price >= ? AND unit_price <= ?', min_price, max_price)
+  end
 end
