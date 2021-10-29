a033ca4d26adeff503fcb6aedd3bc5c0412cc816
Ida Olson <idaolson@gmail.com>
2021-10-29 00:11:59 -0600

    Add sad path empty method to merchant serializer

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -2,4 +2,14 @@ class MerchantSerializer
   include JSONAPI::Serializer
 
   attributes :name
+
+  def self.empty
+    { data:
+      { "id": nil,
+        "type": 'merchant',
+        "attributes":
+         { "name": nil }
+      }
+    }
+  end 
 end
